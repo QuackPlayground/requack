da534a349a4b29018f2d2918209aa2411f154799
pockypoem <jeremyas.cornelis@gmail.com>
2024-01-15 12:29:29 +0700

    feat: login form without atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,30 @@ import Button from "./components/Elements/Button"
 function App() {
 
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div className="flex gap-x-3">
-        <Button variant="bg-red-700">Login</Button>
-        <Button variant="bg-slate-600">Logout</Button>
-        <Button variant="bg-black">Register</Button>
-        <Button></Button>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-xs">
+        <h1 className="mb-2 text-3xl font-bold text-blue-600">Login</h1>
+        <p className="mb-8 font-medium text-slate-500">
+          Welcome, Please enter your details
+        </p>
+        <form action="">
+          <div className="mb-6">
+            <label htmlFor="email" className="block mb-2 text-sm font-bold text-slate-700">
+              Email
+            </label>
+            <input type="email" className="w-full px-3 py-2 text-sm border rounded text-slate-700 placeholder:opacity-50" placeholder="example@mail.com"/>
+          </div>
+
+          <div className="mb-6">
+            <label htmlFor="password" className="block mb-2 text-sm font-bold text-slate-700">
+              Password
+            </label>
+            <input type="password" className="w-full px-3 py-2 text-sm border rounded text-slate-700 placeholder:opacity-50" placeholder="*****"/>
+          </div>
+
+          <Button classname="w-full bg-blue-600">Login</Button>
+
+        </form>
       </div>
     </div>
   )
--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 const Button= (props) => {
 
-    const {children='Default', variant = 'bg-black'} = props
+    const {children='Default', classname = 'bg-black'} = props
   
     return (
       // eslint-disable-next-line react/prop-types
-      <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+      <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} type="submit">
           {children}
       </button>
     )
