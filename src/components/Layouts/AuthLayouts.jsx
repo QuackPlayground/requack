bcf2abb6052b501e28b430dbecf989feaa64b3a2
pockypoem <jeremyas.cornelis@gmail.com>
2024-01-15 14:22:36 +0700

    feat: add react router

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -4,12 +4,14 @@ const AuthLayout = (props) => {
     const {title, children} = props;
 
     return (
-        <div className="w-full max-w-xs">
-            <h1 className="mb-2 text-3xl font-bold text-blue-600">{title}</h1>
-            <p className="mb-8 font-medium text-slate-500">
-            Welcome, Please enter your details
-            </p>
-            {children}
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="w-full max-w-xs">
+                <h1 className="mb-2 text-3xl font-bold text-blue-600">{title}</h1>
+                <p className="mb-8 font-medium text-slate-500">
+                Welcome, Please enter your details
+                </p>
+                {children}
+            </div>
         </div>
     )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Link } from "react-router-dom";
 import FormLogin from "../components/Fragments/FormLogin";
 import AuthLayout from "../components/Layouts/AuthLayouts";
 
@@ -5,6 +7,7 @@ const LoginPage = () => {
     return (
         <AuthLayout title="Login">
             <FormLogin />
+            <p className="mt-5 text-sm text-center">Don't have an acount? <Link to="/register" className="font-bold text-blue-600">Register</Link></p>
         </AuthLayout>
     )
 }
