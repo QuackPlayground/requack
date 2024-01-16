b4875f7f2a2a97f7e66aa6b09d4b1e3eee82f035
pockypoem <jeremyas.cornelis@gmail.com>
2024-01-16 13:57:26 +0700

    feat: nested component

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,23 +1,23 @@
-import Button from "../components/Elements/Button";
+import CardProduct from "../components/Fragments/CardProduct";
 
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <div className="w-full max-w-sm bg-gray-800 border-gray-700 rounded-lg shadow">
-                <a href="#">
-                    <img src="/images/shoes-1.jpg" alt="product" className="p-8 rounded-t-lg"/>
-                </a>
-                <div className="px-5 pb-5">
-                    <a href="#">
-                        <h5 className="text-xl font-semibold tracking-tight text-white">Sepatu Baru</h5>
-                        <p className="text-white text-m">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis assumenda magni autem doloremque consequatur molestias quasi nobis corporis? Consectetur, perspiciatis.</p>
-                    </a>
-                </div>
-                <div className="flex items-center justify-between px-5 pb-5">
-                    <span className="text-xl font-bold text-white">Rp 1.000.000</span>
-                    <Button classname="bg-blue-600">Add To Cart</Button>
-                </div>
-            </div>
+            <CardProduct>
+                <CardProduct.Header image="/images/shoes-1.jpg"/>
+                <CardProduct.Body title="Sepatu Baru">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis assumenda magni autem doloremque consequatur molestias quasi nobis corporis? Consectetur, perspiciatis.
+                </CardProduct.Body>
+                <CardProduct.Footer price="Rp 1.000.000" />
+            </CardProduct>
+
+            <CardProduct>
+                <CardProduct.Header image="/images/shoes-1.jpg"/>
+                <CardProduct.Body title="Sepatu Baru">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis assumenda magni autem doloremque consequatur molestias quasi nobis corporis? Consectetur, perspiciatis.
+                </CardProduct.Body>
+                <CardProduct.Footer price="Rp 1.000.000" />
+            </CardProduct>
         </div>
     )
 }
