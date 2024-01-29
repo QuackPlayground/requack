65e36c935bcfebc6d034daaf121c2ea1a15822b5
pockypoem <jeremyas.cornelis@gmail.com>
2024-01-29 12:07:41 +0700

    feat: useEffect()

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-key */
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button";
-import { useState } from "react";
-import Counter from "../components/Fragments/Counter";
+import { useEffect, useState } from "react";
+// import Counter from "../components/Fragments/Counter";
 
 const products = [
     {
@@ -32,12 +32,22 @@ const email = localStorage.getItem('email');
 
 const ProductsPage = () => {
 
-    const [cart, setCart] = useState([
-        {
-            id: 1,
-            qty: 1
+    const [cart, setCart] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0);
+    useEffect(() => {
+        setCart(JSON.parse(localStorage.getItem("cart")) || [])
+    }, [])
+
+    useEffect(() => {
+        if(cart.length > 0) {
+            const sum = cart.reduce((acc, item) => {
+                const product = products.find((product) => product.id === item.id)
+                return acc + product.price * item.qty
+             }, 0)
+             setTotalPrice(sum);
+             localStorage.setItem("cart", JSON.stringify(cart));
         }
-    ])
+    }, [cart])
 
     const handleLogout = () => {
         localStorage.removeItem('email');
@@ -96,15 +106,19 @@ const ProductsPage = () => {
                                     </tr>
                                 )
                             })}
+                            <tr>
+                                <td colSpan={3}><b>Total Price</b></td>
+                                <td><b>Rp {totalPrice.toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}</b></td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
-            <div className="flex justify-center mt-5 mb-5">
+            {/* <div className="flex justify-center mt-5 mb-5">
                 <Counter></Counter>
-            </div>
+            </div> */}
         </>
     )
 }
 
-export default ProductsPage;
+export default ProductsPage
